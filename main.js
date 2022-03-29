fe2774e607ec9e61d8e5d495ed33a479559b973c
bossteeh <akinrinolaelijah@gmail.com>
2022-03-29 19:50:06 +0100

    added toggle on the bar

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //Javascript
 
   document.getElementById("hamBurger").onclick = function() {
+    //   document.getElementById("hamBurger").classList.toggle("fa-times")
+    hamBurger.classList.toggle("fa-times")
+
       document.getElementById("topNav").classList.toggle("show")
   }
 
 // Alternative Code
-//   hamBurger.addEventListener('click',()=>topNav.classList.toggle('show') )
+ // hamBurger.addEventListener('click',()=>topNav.classList.toggle('show') )
+
   
